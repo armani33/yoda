e201ebcb181f8cafc54e9acab07c4a66a96ee8dd
armand chariot <armand@chariot.be>
2019-04-09 15:13:20 +0200

    creating an independent mobile view, puting rails variant size device in page controller, styling mobile devices

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,31 @@
 class PagesController < ApplicationController
+  before_action :set_variant, only: :home
+
   def home
+    respond_to do |format|
+      format.html(&:phone)
+      # do |html|
+      # html.phone
+      # html.tablet
+      # end
+    end
     @subscriber = Subscriber.new
+  end
+
+  private
+
+  def set_variant
+    case request.user_agent
+    when /iPhone/i
+      request.variant = :phone
+    when /Android/i && /mobile/i
+      request.variant = :phone
+    when /Windows Phone/i
+      request.variant = :phone
+    # when /Android/i
+    #   request.variant = :tablet
+    # when /iPad/i
+    #   request.variant = :tablet
+    end
   end
 end
