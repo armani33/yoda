b01fae94113782d1d48d8bdc0aa85ff288f3c812
armand chariot <armand@chariot.be>
2019-04-10 15:19:06 +0200

    delete config production actionmailer for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "yodacity.herokuapp.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
