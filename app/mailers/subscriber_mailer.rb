9a8f6bb6abfffec9bf86f0815ef88f0d8f4ff223
armand chariot <armand@chariot.be>
2019-04-11 16:09:52 +0200

    creating contact page, and the footer, as well as styling those both page for website and mobile

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,13 +1,7 @@
 class SubscriberMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.subscriber_mailer.welcome.subject
-  #
   def success_to_subscribe(subscriber)
     @subscriber = subscriber
 
-    mail to: @subscriber.email, subject: "Thank you #{@subscriber.name.capitalize} for your subscription! Yoda is coming soon"
+    mail to: @subscriber.email, subject: "Thank you #{@subscriber.name.capitalize } for your subscription! Yoda is coming soon"
   end
 end
